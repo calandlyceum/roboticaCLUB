3e362fd0baab50d4fad97808b9b434c98d4ea908
calandlyceum <41421187+calandlyceum@users.noreply.github.com>
2018-07-19 17:28:45 +0200

    Usability-features toegevoegd
    Hierdoor is het makkelijker om te vliegen.

--- ESC-6motoren-opslaan-met-sturen.py
+++ ESC-6motoren-opslaan-met-sturen.py
@@ -1,19 +1,13 @@
-# This program will let you test your ESC and brushless motor.
-# Make sure your battery is not connected if you are going to calibrate it at first.
-# Since you are testing your motor, I hope you don't have your propeller attached to it otherwise you are in trouble my friend...?
-# This program is made by AGT @instructable.com. DO NOT REPUBLISH THIS PROGRAM... actually the program itself is harmful
-
 # op 11.1 volt is 1250 zweven
-
-import os     #importing os library so as to communicate with the system
-import time   #importing time library to make Rpi wait because its too impatient 
+import os
+import time
 os.system ("sudo pigpiod") #Launching GPIO library
-time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
-import pigpio #importing GPIO library
+time.sleep(1)
+import pigpio
 
 
 
-ESC=23#Connect the ESC in this GPIO pin
+ESC=23
 ESC2=18
 ESC3=25
 ESC4=24
@@ -32,13 +26,13 @@ pi.set_servo_pulsewidth(ESC6, 0)
 error = "Dit is een error."
 beta = "Dit is nog in de beta!"
 
-max_value = 1400 #change this if your ESC's max value is different or leave it be
-min_value = 700  #change this if your ESC's min value is different or leave it be
+max_value = 1400
+min_value = 700
 print("Als je de eerste keer opstart, selecteer dan calibrate")
 print ("Wees wel precies")
 print ("calibrate OF manual OF control OF arm OF stop OF auto (Autiomatisch)")
-def manual_drive(): #You will use this function to program your ESC if required
-    print ("You have selected manual option so give a value between 0 and you max value"    )
+def manual_drive():
+    print ("Manual geselecteerd - geef een waarde tussen 0 en de max-waarde"    )
     while True:
         inp = input()
         if inp == "stop":
@@ -58,14 +52,14 @@ def manual_drive(): #You will use this function to program your ESC if required
             pi.set_servo_pulsewidth(ESC5,inp)
             pi.set_servo_pulsewidth(ESC6,inp)
                 
-def calibrate():   #This is the auto calibration procedure of a normal ESC
+def calibrate():
     pi.set_servo_pulsewidth(ESC, 0)
     pi.set_servo_pulsewidth(ESC2, 0)
     pi.set_servo_pulsewidth(ESC3, 0)
     pi.set_servo_pulsewidth(ESC4, 0)
     pi.set_servo_pulsewidth(ESC5, 0)
     pi.set_servo_pulsewidth(ESC6, 0)
-    print("Disconnect the battery and press Enter")
+    print("Koppel de batterij af en druk op enter")
     inp = input()
     if inp == '':
         pi.set_servo_pulsewidth(ESC, max_value)
@@ -74,7 +68,7 @@ def calibrate():   #This is the auto calibration procedure of a normal ESC
         pi.set_servo_pulsewidth(ESC4, max_value)
         pi.set_servo_pulsewidth(ESC5, max_value)
         pi.set_servo_pulsewidth(ESC6, max_value)
-        print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
+        print("Sluit de batterij aan, wacht op de piepjes en druk op Enter.")
         inp = input()
         if inp == '':            
             pi.set_servo_pulsewidth(ESC, min_value)
@@ -83,24 +77,24 @@ def calibrate():   #This is the auto calibration procedure of a normal ESC
             pi.set_servo_pulsewidth(ESC4, min_value)
             pi.set_servo_pulsewidth(ESC5, min_value)
             pi.set_servo_pulsewidth(ESC6, min_value)
-            print ("Wierd eh! Special tone")
-            print ("Wait for it ....")
-            print ("Im working on it, DONT WORRY JUST WAIT.....")
+            print ("Even wachten...")
+            print ("Nog even wachten...")
+            print ("PIZZA!")
             pi.set_servo_pulsewidth(ESC, 0)
             pi.set_servo_pulsewidth(ESC2, 0)
             pi.set_servo_pulsewidth(ESC3, 0)
             pi.set_servo_pulsewidth(ESC4, 0)
             pi.set_servo_pulsewidth(ESC5, 0)
             pi.set_servo_pulsewidth(ESC6, 0)
-            print ("Arming ESC now...")
+            print ("INITIAL WHOEMM")
             pi.set_servo_pulsewidth(ESC, min_value)
             pi.set_servo_pulsewidth(ESC2, min_value)
             pi.set_servo_pulsewidth(ESC3, min_value)
             pi.set_servo_pulsewidth(ESC4, min_value)
             pi.set_servo_pulsewidth(ESC5, min_value)
             pi.set_servo_pulsewidth(ESC6, min_value)
-            print ("See.... uhhhhh")
-            arm() # You can change this to any other function you want
+            print ("Ik zei het toch")
+            arm()
 def vooruit():
     global speed
     global speed2
@@ -155,9 +149,10 @@ def rechts_draaien():
     print(beta)
     
 def control(): 
-    print ("I'm Starting the motor, I hope its calibrated and armed, if not restart by giving 'x'")
+    print ("De motor moet nu aangaan!")
     time.sleep(1)
-    speed = 700# change your speed if you want to.... it should be between 700 - 2000
+    #De snelheden hieronder zijn startsnelheden. speed = 700 = uit aan het begin.
+    speed = 700
     speed2 = 700
     speed3 = 700
     speed4 = 700
@@ -171,7 +166,7 @@ def control():
     global speed6
     global ESC5
     global ESC6
-    print ("Controls - a to decrease speed & d to increase speed OR q to decrease a lot of speed & e to increase a lot of speed")
+    print ("Als je de controls wilt snappen: LEES DE CODE!")
     while True:
         pi.set_servo_pulsewidth(ESC, speed)
         pi.set_servo_pulsewidth(ESC2, speed2)
@@ -182,77 +177,77 @@ def control():
         inp = input()
         
         if inp == "q":
-            speed -= 100    # decrementing the speed like hell
+            speed -= 100
             print ("speed = %d" % speed)
         elif inp == "w":    
-            speed += 100    # incrementing the speed like hell
+            speed += 100
             print ("speed = %d" % speed)
         elif inp == "a":
-            speed += 10     # incrementing the speed 
+            speed += 10
             print ("speed = %d" % speed)
         elif inp == "s":
-            speed -= 10     # decrementing the speed
+            speed -= 10
             print ("speed = %d" % speed)
         elif inp == "e":
-            speed2 -= 100    # decrementing the speed like hell
+            speed2 -= 100
             print ("speed2  = %d" % speed2)
         elif inp == "r":    
-            speed2 += 100    # incrementing the speed like hell
+            speed2 += 100
             print ("speed2 = %d" % speed2)
         elif inp == "d":
-            speed2 += 10     # incrementing the speed 
+            speed2 += 10
             print ("speed2 = %d" % speed2)
         elif inp == "f":
-            speed2 -= 10     # decrementing the speed
+            speed2 -= 10
             print ("speed2 = %d" % speed2)
         elif inp == "t":
-            speed3 -= 100    # decrementing the speed like hell
+            speed3 -= 100
             print ("speed3  = %d" % speed3)
         elif inp == "y":  
-            speed3 += 100    # incrementing the speed like hell
+            speed3 += 100
             print ("speed3 = %d" % speed3)
         elif inp == "g":
-            speed3 += 10     # incrementing the speed 
+            speed3 += 10
             print ("speed3 = %d" % speed3)
         elif inp == "h":
-            speed3 -= 10     # decrementing the speed
+            speed3 -= 10
             print ("speed3 = %d" % speed3)
         elif inp == "u":
-            speed4 -= 100    # decrementing the speed like hell
+            speed4 -= 100
             print ("speed4  = %d" % speed4)
         elif inp == "i":  
-            speed4 += 100    # incrementing the speed like hell
+            speed4 += 100
             print ("speed4 = %d" % speed4)
         elif inp == "j":
-            speed4 += 10     # incrementing the speed 
+            speed4 += 10
             print ("speed4 = %d" % speed4)
         elif inp == "k":
-            speed4 -= 10     # decrementing the speed
+            speed4 -= 10
             print ("speed4 = %d" % speed4)
         elif inp == "z":
-            speed5 -= 100    # decrementing the speed like hell
+            speed5 -= 100
             print ("speed5  = %d" % speed5)
         elif inp == "x":  
-            speed5 += 100    # incrementing the speed like hell
+            speed5 += 100
             speed6 += 100
             print ("speed5 = %d" % speed5)
         elif inp == "c":
-            speed5 += 10     # incrementing the speed 
+            speed5 += 10
             print ("speed5 = %d" % speed5)
         elif inp == "v":
-            speed5 -= 10     # decrementing the speed
+            speed5 -= 10
             print ("speed6 = %d" % speed5)
         elif inp == "b":
-            speed6 -= 100    # decrementing the speed like hell
+            speed6 -= 100
             print ("speed6  = %d" % speed6)
         elif inp == "n":  
-            speed6 += 100    # incrementing the speed like hell
+            speed6 += 100
             print ("speed6 = %d" % speed6)
         elif inp == "m":
-            speed6 += 10     # incrementing the speed 
+            speed6 += 10
             print ("speed6 = %d" % speed6)
         elif inp == ",":
-            speed6 -= 10     # decrementing the speed
+            speed6 -= 10
             print ("speed6 = %d" % speed6)
         elif inp == "9":
             speed += 100
@@ -294,7 +289,7 @@ def control():
             speed5 = 700
             speed6 = 700
         elif inp == "stop":
-            stop()          #going for the stop function
+            stop()
             break
         elif inp == "manual":
             manual_drive()
@@ -315,10 +310,10 @@ def control():
         elif inp == "-":
             rechts_draaien()
         else:
-            print ("WHAT DID I SAID!! Press a,q,d or e")
+            print ("Dit is geen geldig antwoord")
             
-def arm(): #This is the arming procedure of an ESC 
-    print ("Connect the battery and press Enter")
+def arm():
+    print ("Sluit de batterij aan en druk op Enter")
     inp = input()    
     if inp == '':
         pi.set_servo_pulsewidth(ESC, 0)
@@ -345,7 +340,7 @@ def arm(): #This is the arming procedure of an ESC
         control() 
 def automatisch():
     calibrate()
-def stop(): #This will stop every action your Pi is performing for ESC ofcourse.
+def stop():
     pi.set_servo_pulsewidth(ESC, 0)
     pi.set_servo_pulsewidth(ESC2, 0)
     pi.set_servo_pulsewidth(ESC3, 0)
@@ -354,7 +349,6 @@ def stop(): #This will stop every action your Pi is performing for ESC ofcourse.
     pi.set_servo_pulsewidth(ESC6, 0)
     pi.stop()
 
-#This is the start of the program actually, to start the function it needs to be initialized before calling... stupid python.    
 inp = input()
 if inp == "manual":
     manual_drive()
@@ -367,4 +361,4 @@ elif inp == "control":
 elif inp == "stop":
     stop()
 else :
-    print ("Thank You for not following the things I'm saying... now you gotta restart the program STUPID!!")
+    print ("Dit is geen geldig antwoord")
